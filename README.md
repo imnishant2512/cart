9357a6727f4cf1a9902cebb25f3433232bf7bdb3
Nishant <nkmr366@gmail.com>
2021-08-31 21:59:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cart
+ A basic cart devloped using React and Firebase
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  const name = 'Nishant';
+  return (
+    <div className="App">
+      <h1>Cart</h1>
+    </div>
+  );
+}
+
+export default App;
