4fdb59c3dfef2c45d99734ea60b1f23717361ac4
Nishant <nkmr366@gmail.com>
2021-09-03 19:50:59 +0530

    Added + and - function for CartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -12,7 +12,29 @@ class CartItem extends React.Component {
     // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
   increaseQuantity = () => {
-    console.log('this', this.state);
+
+    //console.log('this', this.state);
+    this.setState((prevState) =>{
+      return{
+        qty: prevState.qty +1
+      }
+    })
+  }
+  decreaseQuantity = () => {
+
+    const {qty} = this.state;
+
+    if(qty === 0)
+    {
+      return;
+    }
+
+    //console.log('this', this.state);
+    this.setState((prevState) =>{
+      return{
+        qty: prevState.qty -1
+      }
+    })
   }
   render () {
     const { price, title, qty } = this.state;
@@ -37,6 +59,7 @@ class CartItem extends React.Component {
               alt="decrease"
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+              onClick={this.decreaseQuantity}
             />
             <img
               alt="delete"
